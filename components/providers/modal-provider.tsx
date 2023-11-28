47a30c4856072e5ca04f01de084519f09eb5b75f
Maxime BAUDOIN <maxime.baudoin@epsi.fr>
2023-11-28 23:10:29 +0100

    sync

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <CreateServerModal />
+            <EditServerModal />
+            <MembersModal />
+            <InviteModal />
+        </>
+    );
+}
+ 
+export default ModalProvider;
