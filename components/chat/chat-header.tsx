39f636a6a79f9b1d2b2c62e7807b2f66851d8270
Maxime BAUDOIN <maxime.baudoin@epsi.fr>
2023-12-03 17:39:06 +0100

    Socket IO Setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -16,21 +17,26 @@ export const ChatHeader = ({
   type,
   imageUrl
 }: ChatHeaderProps) => {
-  return (
-    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle serverId={serverId} />
-      {type === "channel" && (
-        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
-      )}
-      {type === "conversation" && (
-        <UserAvatar 
-          src={imageUrl}
-          className="h-7 w-7 md:h-7 md:w-7 mr-3"
-        />
-      )}
-      <p className="font-semibold text-md text-black dark:text-white">
-        {name}
-      </p>
-    </div>
-  )
+
+
+    return (
+        <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+            <MobileToggle serverId={serverId} />
+            {type === "channel" && (
+                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+            )}
+            {type === "conversation" && (
+                <UserAvatar 
+                    src={imageUrl}
+                    className="h-7 w-7 md:h-7 md:w-7 mr-3"
+                />
+            )}
+            <p className="font-semibold text-md text-black dark:text-white">
+                {name}
+            </p>
+            <div className="ml-auto flex items-center">
+                <SocketIndicator />
+            </div>
+        </div>
+    )
 }
